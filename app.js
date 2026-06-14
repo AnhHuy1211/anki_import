37ca67ecba49022ccd50ccedc532036da11ac1dc
AnhHuy1211 <nguyen.a.huy03217@gmail.com>
2026-06-14 21:12:09 +0900

    add new line to sentence

--- app.js
+++ app.js
@@ -138,11 +138,15 @@ window.addRow = addRow; window.removeRow = removeRow; window.clearTable = clearT
 window.addEventListener('load', ()=>{
   document.getElementById('check-anki').addEventListener('click', checkAnki);
 
-  // Add </br> on Enter key in table inputs
+  // Add </br> on Enter key in table inputs and sentence textarea
   document.addEventListener('keydown', (e) => {
     if(e.key === 'Enter' && e.target.matches('#words-table input')){
       e.preventDefault();
       e.target.value += '</br>';
+    }
+    if(e.key === 'Enter' && e.target.matches('#words-table textarea')){
+      e.preventDefault();
+      e.target.value += '</br>\n';
     }
   });
 });
